ebcf72acebbe012c58a525f7858ac28075b180df
Kyle <kalm42@gmail.com>
2019-03-06 17:17:39 -0700

    feat content (#1)
    * feat: Prettier eslint config
    
    * chore: Remove excess readme documention
    
    * feat: Add offline and make the site a progressive web application
    
    * Make site a gatsby site

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,32 @@
-import React from "react"
-import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
+import React from 'react'
+import PropTypes from 'prop-types'
+import { StaticQuery, graphql, Link } from 'gatsby'
+import styled from 'styled-components'
 
-import Header from "./header"
-import "./layout.css"
+import Header from './header'
+import './global.css'
+
+const Footer = styled.footer`
+  padding: 1rem;
+  background: #fff41e;
+  margin-top: 1.45rem;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, auto));
+  grid-gap: 20px;
+  @media screen and (max-width: 900px) {
+    grid-template-columns: 1fr;
+  }
+
+  a {
+    color: #000;
+  }
+`
+
+const Wrapper = styled.div`
+  min-height: calc(100vh);
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+`
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -17,24 +40,47 @@ const Layout = ({ children }) => (
       }
     `}
     render={data => (
-      <>
+      <Wrapper>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
+        {children}
+        <Footer>
+          <div>
+            <h5>Contact Me</h5>
+            <address>
+              <a href="mailto:me@kalm42.com">me@kalm42.com</a>
+              <br />
+              <a href="tel:+208-357-9649">(208)357-9649</a>
+              <br />
+              2184 Channing Way # 417
+              <br />
+              Idaho Falls, ID 83404
+            </address>
+            <p>© {new Date().getFullYear()} kalm42, Inc.</p>
+          </div>
+          <div>
+            <h5>Customer Service</h5>
+            <ul>
+              <li>
+                <Link to="/contact">Contact Us</Link>
+              </li>
+              <li>
+                <Link to="/legal">Privacy Policy, Terms, and Conditions</Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h5>Subscribe to my email list</h5>
+            <form
+              name="subscribe"
+              method="post"
+              data-netlify="true"
+              data-netlify-honeypot="bot-field">
+              <input type="text" name="email" placeholder="Your email" />
+              <input type="submit" value="Subscribe" />
+            </form>
+          </div>
+        </Footer>
+      </Wrapper>
     )}
   />
 )
---
+++ src/images/CREDITS.md
@@ -0,0 +1,12 @@
+---
+slug: "/photo-credits"
+title: "Photo Credits"
+date: "2019-02-28"
+---
+
+Photo by Mitchell Kmetz on [Unsplash](https://unsplash.com/photos/aS8sG2McR_o)
+Photo by Peter Gonzalez on [Unsplash](https://unsplash.com/photos/7cDE9FrHwd8)
+Photo by Tim Peterson on [Unsplash](https://unsplash.com/photos/H5O2XwzvEdc)
+Photo by Domenico Loia on [Unsplash](https://unsplash.com/photos/EhTcC9sYXsw)
+Photo by rawpixel on [Unsplash](https://unsplash.com/photos/DWui9DmfCXA)
+Photo by rawpixel.com from [Pexels](https://www.pexels.com/photo/turned-on-black-iphone-7-displaying-hello-1275929/)

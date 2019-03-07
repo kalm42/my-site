192bdaa487f8739bfa3a6b2fa232c7eb113c8987
Kyle <kalm42@gmail.com>
2019-03-06 17:43:16 -0700

    V1.1.2 (#4)
    * Increment number
    
    * Correct import statement

--- src/components/PostLayout.js
+++ src/components/PostLayout.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { graphql } from 'gatsby'
-import Layout from './Layout'
+import Layout from './layout'
 import SEO from './seo'
 import { ContentWrapper } from './styles/shared'
 

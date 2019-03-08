dc85f370f19ddce57a8121315ecd5b047f817658
Kyle <kalm42@gmail.com>
2019-03-08 09:45:19 -0700

    V1.2.0 (#9)
    * Increment version
    
    * Feat/sentry (#7)
    
    * feature: Add gatsby sentry plugin
    
    * feat: Configure Sentry
    
    * chore: Add wildcard to environmental variable files
    
    * feat: Add Error boundary component
    
    * feat: Add ErrorBoundary to top level of layout
    
    * fix: Get all font weights (#8)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,7 @@
 /* eslint template-curly-spacing: 0 */
+require('dotenv').config({
+  path: `.env.production`,
+})
 module.exports = {
   siteMetadata: {
     title: `kalm42`,
@@ -9,7 +12,6 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -28,7 +30,7 @@ module.exports = {
         background_color: `#FFF41E`,
         theme_color: `#FFF41E`,
         display: `minimal-ui`,
-        icon: `src/images/icons/favicon-32x32.png`, // This path is relative to the root of the site.
+        icon: `src/images/icons/favicon-32x32.png`, // This path is relative to the root of the site
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
@@ -45,8 +47,17 @@ module.exports = {
       resolve: 'gatsby-plugin-web-font-loader',
       options: {
         google: {
-          families: ['Roboto'],
+          families: ['Roboto:100,200,300,400,500,600,700,800,900'],
         },
+      },
+    },
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: 'gatsby-plugin-sentry',
+      options: {
+        dsn: process.env.GATSBY_SENTRY_DSN,
+        environment: process.env.NODE_ENV,
+        enabled: (() => ['production', 'stage'].indexOf(process.env.NODE_ENV) !== -1)(),
       },
     },
     `gatsby-transformer-sharp`,
